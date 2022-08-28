93a72bb82cec5154882b59459899496548e70246
Fatih-Cyber <fatih@cybertekschool.com>
2022-08-28 19:36:55 +0300

    first Push

--- 01.AlgorithmAnalysis/src/BigOSingleLoop.java
+++ 01.AlgorithmAnalysis/src/BigOSingleLoop.java
@@ -3,12 +3,16 @@ import java.util.ArrayList;
 public class BigOSingleLoop {
     public static void main(String[] args) {
         int numberOfOperations=0;
-        int n=100;
+        int n=100;// size of  Data
         long startTime;
         long endTime;
         System.out.println("Input size n is = "+ n);
         // Task 1 Single Loop...................
-
+        startTime=System.currentTimeMillis();
+        for (int i = 0; i < n ; i++) {
+            numberOfOperations+=1;
+        }
+        endTime=System.currentTimeMillis();
         // Task 2 Nested Loops...................
 
         // Task 2 End...................
