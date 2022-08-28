40346cd8ca237adf4520b9407b161e7d750ff857
Fatih-Cyber <fatih@cybertekschool.com>
2022-08-28 17:32:49 +0300

    first Push

--- 01.AlgorithmAnalysis/src/BigOSingleLoop.java
+++ 01.AlgorithmAnalysis/src/BigOSingleLoop.java
@@ -1,11 +1,28 @@
+import java.util.ArrayList;
+
 public class BigOSingleLoop {
     public static void main(String[] args) {
-        int numberOperations=0;
+        int numberOfOperations=0;
         int n=100;
-        for (int i = 0; i < n; i++) {
-            numberOperations++;
+        long startTime;
+        long endTime;
+        System.out.println("Input size n is = "+ n);
+        // Task 1 Single Loop...................
 
-        }
-        System.out.println(numberOperations);
+        // Task 2 Nested Loops...................
+
+        // Task 2 End...................
+// ......................................................................................................
+        // Task 3 Three Nested Loops...................
+
+        // Task 3 End...................
+// ......................................................................................................
+        // Task 4 N and M Nested Loops...................
+        // Task 4 End...................
+
+        // Task 5 Logarithmic Complexity...................
+       // Task 5 End...................*/
     }
 }
+
+
