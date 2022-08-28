27452322bda044830bf94d7e3861ea77224575ba
Fatih-Cyber <fatih@cybertekschool.com>
2022-08-28 17:27:19 +0300

    first Push

--- 01.AlgorithmAnalysis/src/BigOSingleLoop.java
+++ 01.AlgorithmAnalysis/src/BigOSingleLoop.java
@@ -1,2 +1,11 @@
 public class BigOSingleLoop {
+    public static void main(String[] args) {
+        int numberOperations=0;
+        int n=100;
+        for (int i = 0; i < n; i++) {
+            numberOperations++;
+
+        }
+        System.out.println(numberOperations);
+    }
 }
