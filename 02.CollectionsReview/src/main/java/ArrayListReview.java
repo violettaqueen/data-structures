944aa930c2cbe333181157d0816126154b7305fc
Fatih-Cyber <fatih@cybertekschool.com>
2022-08-29 19:38:18 +0300

    + Number of People in Bus

--- 02.CollectionsReview/src/main/java/ArrayListReview.java
+++ 02.CollectionsReview/src/main/java/ArrayListReview.java
@@ -3,14 +3,21 @@ import java.util.*;
 public class ArrayListReview {
     public static void main(String[] args) {
         // Create ArrayList and a class
-
+   List<Student> students=new ArrayList<>();
 
 // Add elements to ArrayList
-
+        students.add(new Student(1,"Jack"));
+        students.add(new Student(2,"Julia"));
+        students.add(new Student(3,"Mike"));
+        students.add(new Student(4,"Mary"));
 
         // Iteration on ArrayLists
         // 1. For Loop with get(index)
 
+        System.out.println("Printing with legacy for-loop..............");
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i));
+        }
         // 2. Iterator
         // Forward Iteration
 
