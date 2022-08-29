4b412b9e0ffd8ed5920502ef266b98765c9ded49
Fatih-Cyber <fatih@cybertekschool.com>
2022-08-29 21:19:47 +0300

    + Sets added

--- 02.CollectionsReview/src/main/java/SetReview.java
+++ 02.CollectionsReview/src/main/java/SetReview.java
@@ -1,17 +1,20 @@
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.concurrent.Callable;
 
 public class SetReview {
     public static void main(String[] args) {
         //1. create a set
-
-
-
-
+      Set<Student> set =new LinkedHashSet<>();
         // 2 add element
 
+        set.add(new Student(1,"Jack"));
+        set.add(new Student(2,"Julia"));
+        set.add(new Student(3,"Mike"));
+        set.add(new Student(4,"Mary"));
 
+        System.out.println(set);
 
     }
     public static Character firstRepeatingChar(String str){
