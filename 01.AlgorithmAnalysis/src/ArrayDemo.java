00844890be1d063b981a53754749d257cb33a3d0
Fatih-Cyber <fatih@cybertekschool.com>
2022-08-29 18:17:17 +0300

    + Collection REview Added

--- 01.AlgorithmAnalysis/src/ArrayDemo.java
+++ 01.AlgorithmAnalysis/src/ArrayDemo.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayDemo {
     public static void main(String[] args) {
         int[] scoreArray=new int[10];
-        System.out.println(scoreArray);
+
     }
 }
